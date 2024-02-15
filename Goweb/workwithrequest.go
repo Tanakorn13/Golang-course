e58de516b9c26868f8310b5ce0be09fc6c9e59eb
Tanakorn Pongtongmuang <96484902+Tanakorn13@users.noreply.github.com>
2024-02-16 02:16:54 +0700

    API

---
+++ Goweb/workwithrequest.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Course struct {
+	ID         int     `json:"id"` //custom key
+	Name       string  `json:"name"`
+	Price      float64 `json:"price"`
+	Instructor string  `json:"instructor"`
+}
+
+var CourseList []Course
+
+func init() {
+	CourseJSON := `[
+		{
+			"id": 1,
+			"name": "Golang",
+			"price": 1000,
+			"instructor": "John Doe"
+		},
+		{
+			"id": 2,
+			"name": "Java",
+			"price": 2000,
+			"instructor": "Jane Doe"
+		},
+		{
+			"id": 3,
+			"name": "Python",
+			"price": 3000,
+			"instructor": "Jack Doe"
+		}
+	]`
+	err := json.Unmarshal([]byte(CourseJSON), &CourseList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func getNextId() int {
+	highestID := -1
+	for _, course := range CourseList {
+		if highestID < course.ID {
+			highestID = course.ID
+		}
+	}
+	return highestID + 1
+}
+
+func courseHandler(w http.ResponseWriter, r *http.Request) {
+	courseJSON, err := json.Marshal(CourseList)
+	switch r.Method {
+	case http.MethodGet:
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(courseJSON)
+	case http.MethodPost:
+		var newCourse Course
+		Bodybyte, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(Bodybyte, &newCourse)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if newCourse.ID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newCourse.ID = getNextId()
+		CourseList = append(CourseList, newCourse)
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+}
+func main() {
+	http.HandleFunc("/course", courseHandler)
+	http.ListenAndServe(":5000", nil)
+}
